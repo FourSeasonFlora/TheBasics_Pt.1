4f1ac278f2e0664252ed24f4fa829e6e3bc6965a
FourSeasonFlora <fourseasonflora@gmail.com>
2022-03-23 16:49:27 -0400

    Levels 1-3 completed

--- TheBasics_Pt.1/TheBasics_Pt.1/Program.cs
+++ TheBasics_Pt.1/TheBasics_Pt.1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, Big World!");
+//Console.WriteLine("My name is Lauren,");
+//Console.WriteLine("I like gardening and beer!");
+//Console.WriteLine("I am studdying C#");
+//Console.WriteLine("and it can do anything!");
+
+Console.WriteLine("Bread is ready!");
+Console.WriteLine("Who is the bread for? ");
+var name = Console.ReadLine();
+Console.WriteLine(name + " got bread.");
