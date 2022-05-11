56d858c8ba4e9943959233d822ae59f35241f9c5
FourSeasonFlora <fourseasonflora@gmail.com>
2022-05-11 16:55:41 -0400

    Level 12

--- TheBasics_Pt.1/TheBasics_Pt.1/continued.cs
+++ TheBasics_Pt.1/TheBasics_Pt.1/continued.cs
@@ -1,21 +1,41 @@
 ﻿/* Level 12 - The Replicator of D'To */
 
-int[] replicatorNum = new int[5];
-for (int index = 0; index < 5 ; index++)
-{
-    Console.Write("Please provide a number: ");
-    var userNum = Console.ReadLine();
-    int num = Convert.ToInt32(userNum);
-    replicatorNum[index] = num;
-}
+//int[] replicatorNum = new int[5];
+//for (int index = 0; index < 5 ; index++)
+//{
+//    Console.Write("Please provide a number: ");
+//    var userNum = Console.ReadLine();
+//    int num = Convert.ToInt32(userNum);
+//    replicatorNum[index] = num;
+//}
 
-int[] replicator = new int[5];
-for (int number = 0; number < 5; number++)
-{
-    replicator[number] = replicatorNum[number];
-}
+//int[] replicator = new int[5];
+//for (int number = 0; number < 5; number++)
+//{
+//    replicator[number] = replicatorNum[number];
+//}
 
-for (int number = 0; number < 5; number++)
+//for (int number = 0; number < 5; number++)
+//{
+//    Console.WriteLine($"Old array {replicatorNum[number]} : New {replicator[number]}");
+//}
+
+/* The Laws of Freach */
+int[] ourNumbers = new int[] { 4, 51, 10, 1, 80 };
+
+int curentSmallest = int.MaxValue;
+foreach (int minVal in ourNumbers)
 {
-    Console.WriteLine($"Old array {replicatorNum[number]} : New {replicator[number]}");
+    if (minVal < curentSmallest)
+        curentSmallest = minVal;
 }
+Console.WriteLine(curentSmallest);
+
+int total = 0;
+foreach(int numtotal in ourNumbers)
+{
+    total += numtotal;
+}
+//float sum = (float)total;
+float average = (float)total / ourNumbers.Length;
+Console.WriteLine(average);
